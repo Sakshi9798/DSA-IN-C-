3ecf75ca2be22ef0e1f9db4c3a7d56bf539b2aea
Sakshi9798 <sakshiparjapat1709@gmail.com>
2024-12-27 21:44:42 +0530

    initial commit

---
+++ Basics of C++/Input.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int age; // garbage value
+//     cout<<"enter your age : ";
+//     cin>>age;
+
+//     cout<<"your age is "<<age<<endl;
+//     return 0;
+// }
+
+// sum of a&b
+#include <iostream>
+using namespace std;
+ int main() {
+    int a;
+    int b;
+
+     // input a
+     cout<<"enter a : ";
+     cin>>a;
+
+     //input b
+     cout<<"enter b :";
+     cin>>b;
+
+     //calculate sum
+     int sum = a + b;
+     cout<<" sum "<<sum<<endl;
+     return 0;
+
+ }
