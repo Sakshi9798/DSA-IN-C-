e6701d622bb314962f369d26241ca7c2c6313c2f
Sakshi9798 <sakshiparjapat1709@gmail.com>
2024-12-27 21:06:00 +0530

    initial commit

---
+++ Basics of C++/Fourthprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    int marks = -200;
+    char grade = 'A';
+    bool isAdult = true;
+
+    cout<<age<<" "<<marks<<" "<<grade<<" "<<isAdult<<endl;
+    cout<<"size of int = "<<sizeof(int)<<endl;
+    cout<<"size of char = "<<sizeof(int)<<endl;
+    cout<<"size of int = "<<sizeof(int)<<endl;
+    return 0;
+
+}
