b54172ff915c7a2cd6854ccb68bff15e9e22c1e6
Sakshi9798 <sakshiparjapat1709@gmail.com>
2024-12-27 21:21:31 +0530

    initial commit

---
+++ Basics of C++/Fifthprogram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    float PI = 3.14856789;
+    double PI2 = 3.14894759758;
+
+    cout<<setprecision(12)<<"PI = "<<PI<<endl;
+    cout<<setprecision(12)<<"PI2 = "<<PI2<<endl;
+
+    return 0;
+
+}
