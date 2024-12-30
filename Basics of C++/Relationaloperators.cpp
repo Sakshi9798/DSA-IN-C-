b50a62b03ae52b25a2a43b2f4f95036c5251e7de
Sakshi9798 <sakshiparjapat1709@gmail.com>
2024-12-30 21:36:22 +0530

    initial commit

--- Basics of C++/Relationaloperators.cpp
+++ Basics of C++/Relationaloperators.cpp
@@ -1,10 +1,30 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int a = 3;
+//     int b = 4;
+
+//     cout << (a > b) << endl;
+//     cout << (a < b) << endl;
+//     cout << (a <= b) << endl;
+//     cout << (a >= b) << endl;
+//     cout << (a == b) << endl;
+//     cout << (a != b) << endl;
+
+//     return 0;
+
+    
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
 
-    int a = 3;
-    int b = 4;
+    int a = 30;
+    int b = 47;
 
     cout << (a > b) << endl;
     cout << (a < b) << endl;
