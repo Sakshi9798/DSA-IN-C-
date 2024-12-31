18b3e8752c5fee9f70bc56c9a54400f9677acd33
Sakshi9798 <sakshiparjapat1709@gmail.com>
2024-12-31 12:06:15 +0530

    intial commit

--- Basics of C++/Logicaloperator.cpp
+++ Basics of C++/Logicaloperator.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
 
     cout << ((3 > 5) && (10 > 7)) << endl;
+    cout << ((3 == 5) || (9 > 5)) << endl;
+    cout << (!(67 > 98)) << endl;
+    cout << (!(9 != 7)) << endl;
     return 0;
 
 }
--- Basics of C++/tempCodeRunnerFile.cpp
+++ Basics of C++/tempCodeRunnerFile.cpp
@@ -1,11 +1,2 @@
-#include <iostream>
-// using namespace std;
 
-// int main() {
-//     int age; // garbage value
-//     cout<<"enter your age : ";
-//     cin>>age;
-
-//     cout<<"your age is "<<age<<endl;
-//     return 0;
-// }
+}
