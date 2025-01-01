9d9a5ea2d98733ccbe3b3c33ad78e33fe5549346
Sakshi9798 <sakshiparjapat1709@gmail.com>
2025-01-01 21:10:41 +0530

    initial commit

---
+++ Conditional statements/2program.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+    float marks;
+    cout << "enter your marks : ";
+    cin >>marks;
+
+    if(marks >=33) {
+        cout << "pass" <<endl;
+    }else {
+        cout << "fail" << endl;
+    }
+ }
