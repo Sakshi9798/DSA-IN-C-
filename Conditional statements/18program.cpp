99faf56a3a72de33fa2ff027c0e174fa6f201185
Sakshi9798 <sakshiparjapat1709@gmail.com>
2025-01-04 13:49:35 +0530

    initial commit

---
+++ Conditional statements/18program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// to print number from 1 to n
+    for(int i=1; i<=5; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for(int i=1; i<=10; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
