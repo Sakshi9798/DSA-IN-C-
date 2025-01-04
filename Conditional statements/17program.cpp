0e42f76ae75204142b1c192b1e7e5a04e42c3ef1
Sakshi9798 <sakshiparjapat1709@gmail.com>
2025-01-04 13:39:29 +0530

    initial commit

---
+++ Conditional statements/17program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   int i;
+   for(i=1; i<=5; i++) {
+    cout << "your fault" << endl;
+   }
+
+    cout << endl;
+    cout << "last value of i = " << i << endl;
+   
+    return 0;
+
+}
