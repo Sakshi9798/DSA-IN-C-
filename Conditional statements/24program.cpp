3a0a21fe3234b4dd606586a519fe61c4b6981f8a
Sakshi9798 <sakshiparjapat1709@gmail.com>
2025-01-07 14:03:13 +0530

    initial commit

--- Conditional statements/24program.cpp
+++ Conditional statements/24program.cpp
@@ -3,15 +3,17 @@ using namespace std;
 
 int main() {
 
-    int n = 10829;
+    int n = 5555;
     int digsum = 0;
-    
+
     while(n > 0) {
         int lastdig = n % 10;
-        digsum += lastdig;
-        n = n / 10; // divide every time, to control infinit loop
+        if(lastdig % 2 != 0) {
+            digsum += lastdig;
+        }
+        n = n / 10;
+        
     }
     cout << "sum = " << digsum << endl;
-
-        return 0;
+    return 0;
 }
