52a5811450ab6605685c83f6dccb40fe4dfb6cc0
Sakshi9798 <sakshiparjapat1709@gmail.com>
2025-01-14 13:50:22 +0530

    initial commit

--- Conditional statements/40program.cpp
+++ Conditional statements/40program.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
+    //outer loop
      for(int i=1; i<=4; i++) {
+        // inner loop
         for(int j=1; j<=4; j++) {
             //work
             cout << i << " ";
